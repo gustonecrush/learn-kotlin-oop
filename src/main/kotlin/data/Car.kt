f13797d7a0db6d76597984a55e25e78f129ee109
gustonecrush <codeboxezz@gmail.com>
2022-08-07 11:09:09 +0700

    initializer block in kotlin

--- src/main/kotlin/data/Car.kt
+++ src/main/kotlin/data/Car.kt
@@ -2,6 +2,10 @@ package data
 
 class Car(paramBrand: String, paramYear: Int = 2020) {
 
+    init {
+        println("Car has been created")
+    }
+
     var brand: String = paramBrand
     var year: Int = paramYear
 
--- src/main/kotlin/data/Person.kt
+++ src/main/kotlin/data/Person.kt
@@ -4,6 +4,11 @@ class Person(firstNameParam: String,
              middleNameParam: String?,
              lastNameParam: String) {
 
+    // initializer block
+    init {
+        println("Person has been created")
+    }
+
     // properties
     var firstName: String = firstNameParam
     val middleName: String? = middleNameParam
